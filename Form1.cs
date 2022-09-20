263f417c14076e7d2cc12f2a5ecf3a3d2217e02f
EDB999 <113853336+EDB999@users.noreply.github.com>
2022-09-20 20:47:50 +0300

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_1_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double vremya = Convert.ToDouble(textBox1.Text);
+            if (vremya >= 0 && vremya <= 24)
+            {
+                label1.Text = "Время суток: " + vremya + ":00";
+            }
+            else label1.Text = "Введите значение из заданного промежутка";
+        }
+    
+    }
+}
